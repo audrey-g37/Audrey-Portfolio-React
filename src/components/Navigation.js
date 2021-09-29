a95ed549a1f4291c980f622c214752d889dfeab1
Audrey <audrey.gillies@gmail.com>
2021-09-29 15:47:08 -0600

    component js files created and basic code added

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <>
+      <h1>This is the Navigation</h1>
+    </>
+  );
+}
+
+export default Navigation;
---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+  return (
+    <>
+      <h1>This is the project section</h1>
+    </>
+  );
+}
+
+export default Project;
