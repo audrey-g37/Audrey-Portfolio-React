e0d8c59a662266e2cd6de0cc5bd70a8bb40c2843
Audrey <audrey.gillies@gmail.com>
2021-09-30 09:37:37 -0600

    projects page has 6 projects displayed

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,55 +1,95 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "../../styles/project.css";
 
 function Project() {
   return (
-    <section id="work">
+    <section>
       <h1>Projects</h1>
-      <Row>
-        <figure className="sample-main">
-          <a
-            href="https://github.com/audrey-g37/Team-Profile-Generator-HW"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h4>Team Profile Generator</h4>
-            <img
-              className="img-fluid"
-              src="./images/team-generator-screenshot.png"
-              alt="An example of the file created after using the application."
-            />
-          </a>
-        </figure>
-        <figure className="sample-secondary">
-          <a
-            href="https://havoxprime.github.io/cocktailAndFood/cocktail.html"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h4>Cocktails</h4>
-            <img
-              className="img-fluid"
-              src="./images/cocktail-recipes-screenshot.png"
-              alt="The 'Long Island Iced Tea' is shown in a screenshot as well as the choices for alcohol selection."
-            />
-          </a>
-        </figure>
-        <figure className="sample-secondary">
-          <a
-            href="https://teacher-force.herokuapp.com/login"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h4>Teacher-Force</h4>
-            <img
-              className="img-fluid"
-              src="./images/teacher-force-screenshot.png"
-              alt="The teacher's homepage is shown after a successful login"
-            />
-          </a>
-        </figure>
-      </Row>
+      <Container fluid id="work">
+        <Row>
+          <Col auto className="sample">
+            <a
+              href="https://github.com/audrey-g37/Team-Profile-Generator-HW"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-1">Team Profile Generator</h4>
+              <img
+                src="./images/team-generator-screenshot.png"
+                alt="An example of the file created after using the application."
+              />
+            </a>
+          </Col>
+          <Col auto className="sample">
+            <a
+              href="https://audrey-g37.github.io/Password-Generator-HW/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-4">Password Generator</h4>
+              <img
+                src="./images/password-generator-screenshot.png"
+                alt="A newly generated password after a user answers the alerts"
+              />
+            </a>
+          </Col>
+          <Col auto className="sample">
+            <a
+              href="https://teacher-force.herokuapp.com/login"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-3">Teacher-Force</h4>
+              <img
+                src="./images/teacher-force-screenshot.png"
+                alt="The teacher's homepage is shown after a successful login"
+              />
+            </a>
+          </Col>
+          <Col auto className="sample">
+            <a
+              href="https://havoxprime.github.io/cocktailAndFood/cocktail.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-2">Food and Cocktails</h4>
+              <img
+                src="./images/cocktail-recipes-screenshot.png"
+                alt="The 'Long Island Iced Tea' is shown in a screenshot as well as the choices for alcohol selection."
+              />
+            </a>
+          </Col>
+          <Col auto className="sample">
+            <a
+              href="https://github.com/audrey-g37/Employee-Tracker-HW"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-5">Employee Tracker</h4>
+              <img
+                src="./images/Employee-Tracker-Demo.gif"
+                alt="Process of adding/updating employees is shown"
+              />
+            </a>
+          </Col>
+          <Col auto className="sample">
+            <a
+              href="https://github.com/audrey-g37/Professional-README-Generator-HW"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h4 className="samp-text-6">Readme Generator</h4>
+              <img
+                src="./images/README-Generator-Demo.gif"
+                alt="Process of creating a new README"
+              />
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
