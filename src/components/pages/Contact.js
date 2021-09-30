c77f0df41323393ef2ffc11f2917d8c511932f4b
Audrey <audrey.gillies@gmail.com>
2021-09-30 10:02:44 -0600

    contact form built; still needs some 'required' statements and styling

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import "../../styles/contact.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control type="text" placeholder="Bob Smith" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Your Email</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="message">
+          <Form.Label>Include a Message</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
