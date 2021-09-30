56e8f29abed0b721db324d9fd5b2361391b8a41f
Audrey <audrey.gillies@gmail.com>
2021-09-30 09:51:10 -0600

    footer contains links to my github and linkedIn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "../styles/footer.css";
 
 function Footer() {
   return (
-    <>
-      <h1 className="align-items-end">This is the footer</h1>
-    </>
+    <ul>
+      <li class="link-info">
+        <a
+          href="https://github.com/audrey-g37"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </li>
+      <li class="link-info">
+        <a
+          href="https://www.linkedin.com/in/audrey-gillies-2764341a1"
+          target="no_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+      </li>
+    </ul>
   );
 }
 
