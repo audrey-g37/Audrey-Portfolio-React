37da849a6a5c51968595467d788e147c05a0b716
Audrey Gillies <audrey.gillies@gmail.com>
2023-03-29 12:09:59 -0600

    updating the resume file and adding my fmc github link; making minor changes to font/coloring for now

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,39 +1,39 @@
-import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Col from "react-bootstrap/Col";
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Col from 'react-bootstrap/Col';
 
 function Navigation({ key, handlePageChange }) {
-  return (
-    <Col>
-      <Nav id="page-nav-btns" variant="pills" activeKey={key} onSelect={handlePageChange}>
-        <Nav.Item>
-          <Nav.Link id="nav-links" eventKey="About">
-            About Me
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link id="nav-links" eventKey="Project">
-            Portfolio
-          </Nav.Link>
-        </Nav.Item>
-        {/* <Nav.Item>
-          <Nav.Link id="nav-links" eventKey="Contact">
-            Contact
-          </Nav.Link>
-        </Nav.Item> */}
-        <Nav.Item>
-          <Nav.Link
-            id="nav-links"
-            href="AudreyGillies_Resume_2022.pdf"
-            eventKey="resume"
-            target="_blank"
-          >
-            Résumé
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
-  );
+	return (
+		<Col>
+			<Nav id='page-nav-btns' variant='pills' activeKey={key} onSelect={handlePageChange}>
+				<Nav.Item>
+					<Nav.Link id='nav-links' eventKey='About'>
+						About Me
+					</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link id='nav-links' eventKey='Project'>
+						Portfolio
+					</Nav.Link>
+				</Nav.Item>
+				{/* <Nav.Item>
+					<Nav.Link id='nav-links' eventKey='Contact'>
+						Contact
+					</Nav.Link>
+				</Nav.Item> */}
+				<Nav.Item>
+					<Nav.Link
+						id='nav-links'
+						href='AudreyGillies_Resume2023.pdf'
+						eventKey='resume'
+						target='_blank'
+					>
+						Résumé
+					</Nav.Link>
+				</Nav.Item>
+			</Nav>
+		</Col>
+	);
 }
 
 export default Navigation;
