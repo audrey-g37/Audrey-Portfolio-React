4f14211b2b892ee2c0217706b1ac3cfc31d0444b
Audrey Gillies <audrey.gillies@gmail.com>
2023-04-04 14:40:44 -0600

    refactoring the display of the pages based on nav clicks

--- src/components/PageChange.js
+++ src/components/PageChange.js
@@ -4,29 +4,22 @@ import Header from './Header';
 import Footer from './Footer';
 import About from '../pages/About';
 import Project from '../pages/Project';
-// import Contact from "../pages/Contact";
 import Row from 'react-bootstrap/Row';
 
 export default function PortfolioChange() {
 	const [textKey, setTextKey] = useState('About');
-
-	const handlePageChange = (k) => {
-		console.log({ k });
-		setTextKey(k);
+	const pageViews = {
+		Project: <Project />,
+		About: <About />
 	};
-
-	const renderPage = () => (textKey === 'Project' ? <Project /> : <About />);
 
 	return (
 		<>
 			<Row id='header-nav' className=' d-flex justify-content-between'>
 				<Header />
-				<Navigation
-					textKey={textKey}
-					handlePageChange={(keyText) => handlePageChange(keyText)}
-				/>
+				<Navigation textKey={textKey} handlePageChange={setTextKey} />
 			</Row>
-			{renderPage()}
+			{pageViews[textKey]}
 			<Footer />
 		</>
 	);
