8123df1d3248b41f4651bebf60f7c97cfc598a4e
Audrey Gillies <audrey.gillies@gmail.com>
2023-04-10 21:05:32 -0600

    using consistency for components and adding a click for my name to take the user to the about me page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
-import React from "react";
-import "../styles/header.css";
-import Col from "react-bootstrap/Col";
+import React from 'react';
+import '../styles/header.css';
+import Col from 'react-bootstrap/Col';
+import { Button } from 'react-bootstrap';
 
-function Header() {
-  return (
-    <Col>
-      <h1 id="my-name">Audrey Gillies</h1>
-    </Col>
-  );
-}
+const Header = ({ handlePageChange }) => {
+	return (
+		<Col>
+			<Button variant='outline-*' onClick={() => handlePageChange('About')}>
+				<h1 id='my-name'>Audrey Gillies</h1>
+			</Button>
+		</Col>
+	);
+};
 
 export default Header;
